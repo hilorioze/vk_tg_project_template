383ef68492efa6af020e2e7244b3f7f77302f59f
Yan Hilorioze <hilorioze@gmail.com>
2022-06-16 12:09:24 +0100

    Fix: replace f-string

--- src/custom/error_handler.py
+++ src/custom/error_handler.py
@@ -6,7 +6,7 @@ bot_error_handler = ErrorHandler(redirect_arguments=True)
 
 
 async def bot_error_handler_undefined_error(error: Exception, *args, **kwargs) -> None:
-    logger.exception(f"Undefined error: {error}\nargs: {args}\nkwargs: {kwargs}")
+    logger.exception("Undefined error: {}\nargs: {}\nkwargs: {}", error, args, kwargs)
 
 
 bot_error_handler.register_undefined_error_handler(bot_error_handler_undefined_error)
